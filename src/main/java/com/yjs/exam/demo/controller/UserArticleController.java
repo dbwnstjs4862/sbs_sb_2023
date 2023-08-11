81116528080e3090dc5b079c2830b590211a59e2
dbwnstjs4862 <dbwnstjs4862@gmail.com>
2023-08-11 10:48:25 +0900

    UserArticleController가 가지고 있던 서비스 로직들을 ArticleService로 옮김

--- src/main/java/com/yjs/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/yjs/exam/demo/controller/UserArticleController.java
@@ -1,6 +1,5 @@
 package com.yjs.exam.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,87 +12,26 @@ import com.yjs.exam.demo.vo.Article;
 
 @Controller
 public class UserArticleController {
-	// 인스턴스 변수 시작
 	@Autowired
 	private ArticleService articleService;
-	private int articleLastId;
-	private List<Article> articles;
-	// 인스턴스 변수 끝
-	
-	// 생성자
-	public UserArticleController() {
-		articleLastId = 0;
-		articles = new ArrayList<>();
-		
-		makeTestData();
-	}
-	
-	// 서비스 메서드 시작
-	private void makeTestData() {
-		for ( int i = 1; i <= 10; i++ ) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-			
-			writeArticle(title, body);
-		}
-	}
-	
-	public Article writeArticle(String title, String body) {
-		int id = articleLastId + 1;
-		
-		Article article = new Article(id, title, body);
-		
-		articles.add(article);
-		articleLastId = id;
-		
-		return article;
-	}
-	
-	private Article getArticle(int id) {
-		for ( Article article : articles ) {
-			if ( article.getId() == id ) {
-				return article;
-			}
-		}
-		
-		return null;
-	}
-	
-	private void deleteArticle(int id) {
-		Article article = getArticle(id);
-		
-		articles.remove(article);
-		
-	}
-	
-	private void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-		
-		article.setTitle(title);
-		article.setBody(body);
-		
-	}
-	
-	// 서비스 메서드 끝
 
-	// 액션 메서드 시작
 	@RequestMapping("/user/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		Article article = writeArticle(title, body);
+		Article article = articleService.writeArticle(title, body);
 		return article;
 	}
-	
+
 	@RequestMapping("/user/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		return articles;
+		return articleService.getArticles();
 	}
-	
+
 	@RequestMapping("/user/article/getArticle")
 	@ResponseBody
 	public Object getArticleAction(int id) {
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 
 		if (article == null) {
 			return id + "번 게시물이 존재하지 않습니다.";
@@ -101,33 +39,32 @@ public class UserArticleController {
 
 		return article;
 	}
-	
+
 	@RequestMapping("/user/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
-		Article article = getArticle(id);
-		
-		if ( article == null ) {
+		Article article = articleService.getArticle(id);
+
+		if (article == null) {
 			return id + "번 게시물이 존재하지 않습니다.";
 		}
-		
-		deleteArticle(id);
-		
+
+		articleService.deleteArticle(id);
+
 		return id + "번 게시물이 삭제되었습니다.";
 	}
-	// 액션 메서드 끝
-	
+
 	@RequestMapping("/user/article/doModify")
 	@ResponseBody
 	public String doModify(int id, String title, String body) {
-		Article article = getArticle(id);
-		
-		if ( article == null ) {
+		Article article = articleService.getArticle(id);
+
+		if (article == null) {
 			return id + "번 게시물이 존재하지 않습니다.";
 		}
-		
-		modifyArticle(id, title, body);
-		
+
+		articleService.modifyArticle(id, title, body);
+
 		return id + "번 게시물이 수정되었습니다.";
 	}
 }
--- src/main/java/com/yjs/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/yjs/exam/demo/controller/UserHomeController.java
@@ -6,23 +6,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserHomeController {
-	private int count;
-	
-	public UserHomeController() {
-		count = 0;
-	}
-
-	@RequestMapping("/user/home/getCount")
+	@RequestMapping("/user/home/main")
 	@ResponseBody
-	public int getCount() {		
-		return count++;
+	public String getString() {
+		return "메인 입니다";
 	}
-	
-	@RequestMapping("/user/home/doSetCount")
-	@ResponseBody
-	public String doSetCount(int count) {		
-		this.count = count;
-		return "count의 값이 " + this.count + "으로 초기화 되었습니다.";
-	}
-	
 }
