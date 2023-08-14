bf5a99f9323c8d9350ab7b064873dfbfa6e6cfc9
dbwnstjs4862 <dbwnstjs4862@gmail.com>
2023-08-14 10:43:36 +0900

    회원가입을 처리하기 위해, UserMemberController, MemberService 추가

---
+++ src/main/java/com/yjs/exam/demo/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.yjs.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+	public void Join(String loginId, String loginPw, String name, String nickName, String cellphoneNo, String email) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
